959d7aaac5f429129613ee3e6dee23e355276e01
rmabow03 <107537706+rmabow03@users.noreply.github.com>
2024-11-07 16:04:40 +0000

    fixed the code that error or give the incorrect result

--- test_calculator.py
+++ test_calculator.py
@@ -8,8 +8,29 @@ class TestCalculator(unittest.TestCase):
 
     def test_add(self):
         self.assertEqual(self.calc.add(1, 2), 3)
+    def test_add(self):
+        self.assertEqual(self.calc.add(4, 6), 10)
 
     # Add the following test methods to the TestCalculator class:
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(5, 3), 2)
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(2, 9), -7)
+
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(2, 4), 8)
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(4, 3), 12)
+
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(10, 2), 5)
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(10, 2), 5)
+
+    def test_modulo(self):
+       self.assertEqual(self.calc.modulo(3, 5), 0)
+    def test_modulo(self):
+       self.assertEqual(self.calc.modulo(10, 6), 4)
 
 if __name__ == '__main__':
     unittest.main()
